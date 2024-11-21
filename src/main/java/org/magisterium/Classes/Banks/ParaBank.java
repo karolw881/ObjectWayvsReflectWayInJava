37d20e7184679d7dd8ac585b151a9d41a9ef5a46
karol <karolw881@gmail.com>
2024-11-21 13:31:59 +0100

    poprawiona nazwa na ParaBank

--- src/main/java/org/magisterium/Classes/Banks/ParaBank.java
+++ src/main/java/org/magisterium/Classes/Banks/ParaBank.java
@@ -1,8 +1,8 @@
 package org.magisterium.Classes.Banks;
 import org.magisterium.Interfaces.BankAccount;
 
-class Parabank extends Bank implements BankAccount {
-    public Parabank(double initialBalance, String username, String password) {
+class ParaBank extends Bank implements BankAccount {
+    public ParaBank(double initialBalance, String username, String password) {
         super(initialBalance, username, password);
     }
     @Override public String getAdditionalInfo() { return "Parabank - bank prywatny."; }
